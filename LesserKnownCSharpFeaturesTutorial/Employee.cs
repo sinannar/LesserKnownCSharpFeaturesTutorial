7cf8baf3f06a28c7314d66bab772617e8882a5b7
Sinan NAR <sinan.nar@gmail.com>
2018-01-25 12:12:48 +1300

    some steps are done

---
+++ LesserKnownCSharpFeaturesTutorial/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesserKnownCSharpFeaturesTutorial
+{
+    /*
+     * DebuggerDisplay attribute controls how a  type or member is displayed in the debugger windows. 
+     * For example, consider the below class with DebuggerDisplay attribute.
+    */
+    [DebuggerDisplay("Name of employee is {FirstName} {Surname} and his department is {Department}")]
+    class Employee
+    {
+        /*
+         * DebuggerBrowser attribute can be used to further control how the properties and fields appear in the debugger window. 
+         * This attribute takes DebuggerBrowserState enum, which defines  states: Never, Collapsed and RootHidden
+         */
+        [DebuggerBrowsable(DebuggerBrowsableState.Collapsed)]
+        public string FirstName { get; set; }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public string Surname { get; set; }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public string Department { get; set; }
+    }
+}
--- LesserKnownCSharpFeaturesTutorial/Program.cs
+++ LesserKnownCSharpFeaturesTutorial/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+    In this tutorial, I am following this tutorials
+    ->    https://ankitvijay.net/2018/01/08/lesser-known-c-features-part-1/
+
+     */
+
 namespace LesserKnownCSharpFeaturesTutorial
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var employee = new Employee()
+            {
+                FirstName = "John",
+                Surname = "Doe",
+                Department ="Finance"
+            };
         }
     }
 }
